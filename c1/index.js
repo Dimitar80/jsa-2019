379f212ea5c23cce58ca6b5837d431bfb6a6fa9c
kristian3 <kristianjanevski@yahoo.com>
2019-06-12 22:06:20 +0200

    Cas-01

---
+++ c1/index.js
@@ -0,0 +1,25 @@
+console.log('Helloz!');
+
+ime = 'Bojan';
+var prezime = 'Bojanovski';
+let vozrast = 36;
+
+console.log(ime);
+
+console.log(`mkm ${prezime} kmkm`);
+
+// function test(){
+//     console.log(ime);
+//     console.log(prezime);
+//     console.log(vozrast);
+// }
+// test();
+// console.log(test);
+
+function test(){
+    console.log(ime);
+    console.log(prezime);
+    console.log(vozrast);
+}
+test();
+console.log(test);
