6bf16203c02581ad34d7c74c49da7d97adc5d83c
Dimitar80 <designcrafts.mk@gmail.com>
2019-06-20 20:08:54 +0200

    ch

--- c3/v1.js
+++ c3/v1.js
@@ -36,5 +36,72 @@ var students = [
 
   studentsDetails(students);
 
+  var str = "Hello, World";
+  var l = str;
+  console.log(l); 
 
-  
+
+  // function homework2Details(s) {
+//         console.log(s);
+        
+//     var sorted = s.sort((a, b) => a.prosek - b.prosek);
+//         console.log(sorted);
+
+//     console.log(`Student so najnizok prosek ${sorted[0].ime} ${sorted[0].prezime}
+//         so prosek ${sorted[0].prosek}`);
+
+//         console.log(`Student so najnizok prosek ${sorted[sorted.length -1].ime} ${sorted[sorted.length -1].prezime}
+//         so prosek ${sorted[sorted.length -1].prosek}`);
+
+//         var avg = sorted.map((v) => v.prosek).reduce((a,b)=>a+b) / sorted.length;
+//         console.log(`Sreden prosek na generacijata e ${avg}`);
+//   }
+
+//   studentsDetails(students);
+
+
+// console.log(" ");
+
+// console.log("Words with more than 7 letters");
+// function moreThen7(str) {
+//     var strSplit = str.split(' ');
+//     // console.log(strSplit);
+   
+//     var br = 0;
+//     for(var i = 0; i < strSplit.length; i++){
+//       if(strSplit[i].length > 7;){
+//          br ++;
+//          //   console.log(strSplit[i].length);
+//        }
+//     }
+//     return br ++;
+//   }
+ 
+//   moreThen7(lipsum);
+//   console.log(moreThen7(lipsum));
+
+
+
+
+
+//   console.log(" ");
+
+//   console.log("Words with more than 7 letters");
+//   function moreThen7(str) {
+//       var strSplit = str.split(' ');
+//       // console.log(strSplit);
+//       var longestWord = 7;
+//       var br = 0;
+//       for(var i = 0; i < strSplit.length; i++){
+//         if(strSplit[i].length > longestWord){
+//            br ++;
+//           }
+//           // else if(strSplit[i].length < longestWord){
+//           //     br ++;
+//           //    }
+//       }
+//       return br ++;
+//     }
+   
+//     moreThen7(lipsum);
+//     console.log(moreThen7(lipsum));
--- c3/v2.js
+++ c3/v2.js
@@ -66,29 +66,133 @@ var fs = require('fs')
 
 
 
-fs.writeFile('homeWork2.txt', 'Hello World!', function (err) { 
-                        if (err)
-        console.log(err);
-                        else
-        console.log('Write operation complete.');
-});
+// fs.writeFile('test.txt', 'Hello World!', function (err) { 
+//                         if (err)
+//         console.log(err);
+//                         else
+//         console.log('Write operation complete.');
+// });
 
 
-fs.writeFile('test.txt', 'Node.js is the best', (err) => {
-    if(err){
-        console.error(err);
-    }
-});
+
+
+// homework2
+
+var lipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in sem ante. 
+Morbi vulputate, lorem ac pulvinar porttitor, enim nunc tempus diam, et fermentum odio purus ac justo.
+ Cras fringilla nulla in consequat placerat. Donec sollicitudin orci elit, ut dapibus purus mollis egestas.
+  Suspendisse ultricies nec massa vitae mollis. In diam odio, convallis vitae pharetra ut, rhoncus eget purus.
+   Etiam molestie eros at massa euismod suscipit. Nam congue ex vulputate tincidunt suscipit. 
+   Pellentesque mattis tincidunt orci, in pellentesque nisl consectetur a. Mauris at placerat lacus.
+    Phasellus fermentum commodo tellus, in posuere orci elementum interdum. Nunc quis consectetur libero.
+     Vivamus eu purus vitae tortor vestibulum blandit eu sed metus. 
+     Curabitur porta ultrices lectus, sed venenatis urna finibus eu.
+      Curabitur congue pulvinar elit, in viverra orci egestas eu.
+
+Curabitur consequat faucibus tellus nec eleifend. Suspendisse euismod placerat metus.
+ Duis sit amet ornare sapien. Mauris est ex, pulvinar a egestas vel, pulvinar eu orci. 
+ Praesent nec lacus volutpat diam commodo suscipit ut eu ex. Suspendisse eu laoreet augue.
+  Vestibulum tortor mi, ultrices sit amet tempus nec, dignissim eu sem. Pellentesque in convallis lorem.
+   Aliquam in quam eu elit mollis egestas.
+
+Etiam non varius ligula. Praesent finibus dui elit.
+ Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse potenti.
+  Proin sit amet aliquet dui. Fusce non dui sit amet eros efficitur pulvinar ut at orci.
+   Pellentesque vitae consequat elit, eget mattis leo. 
+   Sed gravida urna efficitur, aliquam urna at, porta nisi. Sed eu ipsum libero
+   . Phasellus rutrum nisi risus, eget maximus purus accumsan sit amet. 
+   Pellentesque diam purus, rhoncus ut mi in, mollis aliquet orci. 
+   Quisque a erat non odio sagittis blandit non in ligula. Donec vitae odio elit.
+    Suspendisse fringilla dapibus feugiat. Proin in venenatis dui. Nullam sed accumsan ante.
+
+Mauris sit amet scelerisque arcu. Integer dictum ultricies sodales. 
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+Nam sagittis convallis mi, vitae faucibus diam blandit sed. 
+Vestibulum ac faucibus est. Ut lacus metus, sollicitudin luctus ligula vitae, feugiat facilisis neque.
+ In ac pellentesque neque. Quisque purus tortor, cursus eu metus et, dapibus tempus ipsum. 
+ Cras id eros euismod ante molestie porttitor nec a leo. 
+ Nunc leo orci, facilisis sit amet laoreet eget, rhoncus nec lorem. 
+ Cras semper nisl vel dui ornare, quis vulputate lorem dictum. 
+ Phasellus blandit mi elit, sit amet pretium elit lobortis finibus.
+
+Sed a odio vel mi volutpat ultricies eu sed elit. Donec auctor augue eu dapibus fermentum.
+ Ut consectetur lacus eros, et lobortis quam finibus ac.
+  Aenean iaculis risus lorem, sit amet egestas libero luctus at. 
+  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat vitae lectus vitae congue. 
+   Fusce at felis sit amet risus volutpat iaculis non id nisi. Sed id congue felis.`;
+
+// fs.writeFile('homework2.txt', lipsum, (err) => {
+//     if(err){
+//         console.error(err);
+//     }
+// });
 
    
+// fs.readFile('homework2.txt', 'utf8', (err, lipsum)=>{
+//     if(err){
+//         // console.log(err);
+//         console.error(err);
+//     }
+//     console.log(err);
+//     console.log(lipsum);
+// });
 
 
 
-fs.readFile('homeWork2', 'utf8', (err, data)=>{
-    if(err){
-        // console.log(err);
-        console.error(err);
-    }
-    console.log(err);
-    console.log(data);
-});
+  
+  console.log(" ");
+
+console.log("Words with more than 7 letters");
+
+function w7(str) {
+    var strSplit = str.split(' ');
+    var equ = [];
+    var bigg = [];
+    var less = [];
+    // console.log(strSplit);
+     var br = 0;
+    for(var i = 0; i < strSplit.length; i++){
+      if(strSplit[i].length > 7){
+         br ++
+        };
+        //  if(strSplit[i].length < 7){
+        //     br ++;
+        //    };
+    };
+      return br ++;     
+};
+  
+ w7(lipsum);
+ console.log(w7(lipsum));
+
+ 
+  
+//   var res = br + strSplit.length;
+
+// console.log(" ");
+
+// console.log("Letters");
+// var s = lipsum;
+// var rx = /[a-z]/gi;
+// var m = s.match(rx);
+// if (m) {
+//  console.log(m.length);
+// }
+
+
+// console.log(" ");
+
+// console.log("Words");
+// function WordCount(str) { 
+//     return str.split(" ").length;
+//   }
+  
+//   console.log(WordCount(lipsum));
+
+
+
+
+
+
+
