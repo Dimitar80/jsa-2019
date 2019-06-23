db11a8338461ddddfe26f31542312f8c8e7206e7
Dimitar80 <designcrafts.mk@gmail.com>
2019-06-23 23:08:57 +0200

    5

--- c3/v5exc.js
+++ c3/v5exc.js
@@ -77,9 +77,201 @@
 
 // console.log(result);
 
-for( var  i = 0; i < 5; i++){
-    i += 3
-    console.log(i)
-}
-console.log(i)
+// for( var  i = 0; i < 5; i++){
+//     // i += 1
+//     console.log(i)
+// }
+// console.log(i)
 
+// printReverse start
+
+// function printReverse(arr){
+//     for(var i = arr.length - 1; i >=0; i-- ){
+//         console.log(arr[i]);
+//     }
+// }
+
+// printReverse([3, 5, 8]);
+//     console.log(printReverse[3, 5, 8])
+
+    // printReverse end
+
+
+
+    // isUniform() start
+function isUniform(arr){
+    var first = arr[0];
+    for( var i = 1; i < arr.length; i++){
+        if(arr[i] !== first){
+            return false;
+            }
+        }
+    return true
+}
+
+isUniform([1, 1, 1]);
+console.log(isUniform([1, 1, 1]));
+
+
+
+
+
+var fib = (a,b) => {
+    if(a > 200) {
+      return a + b;
+    }else {
+   console.log("a= " + a);
+       console.log("b= " + b);
+      console.log("zbir a + b = " + (a+b));
+       
+      return fib(b, a+b);
+    }
+   }
+   
+   fib(0,1);
+
+// var fib = (a,b) => {
+//     if(a > 100) {
+//       return a + b;
+//     }else {
+//     //   console.log(a+b);
+//       return fib(b, a+b);
+     
+//     }
+//    }
+//    fib(0,1);
+//    console.log(fib(0,1));
+//    epa b stanuva a, oti e staveno na mestoto na a
+
+// 0+1 = 1, b = 1
+// return (1, 1+1)
+// (1,2)
+
+// b = 3
+// return (3, 3+2)
+// b = 5
+
+
+
+
+// var fibonacci_series = function (n) 
+// {
+//   if (n===1) 
+//   {
+//     return [0, 1];
+//   } 
+//   else 
+//   {
+//     var s = fibonacci_series(n - 1);
+//     s.push(s[s.length - 1] + s[s.length - 2]);
+//     return s;
+//   }
+// };
+
+//  console.log(fibonacci_series(9));
+
+
+
+// console.log(" ")
+// // sumArray start
+//  function sumArray(arr){
+//      var total = 0;
+//      arr.forEach(function(el){
+//          total +=el;
+//      });
+//      return total
+//  }
+
+//  sumArray([2,3,7])
+//  console.log(sumArray([2,3,7]))
+// sumArray end
+
+// max()
+
+// function max(arr){
+//     var max = arr[0]
+//     for(var i =0; i < arr.length; i++){
+//         if(arr[i] > max){
+//             max = arr[i]
+//             // console.log(max)
+//              console.log(arr[i])
+//         }
+//         // console.log(arr[i])
+        
+//     }
+//     // console.log(max)
+//     return max
+     
+// }
+// max([4,2,6,3,14])
+// console.log(max([4,2,6,3,14]))
+
+// function dime(name){
+//     console.log(name + 13 *3 + " years old")
+    
+// }
+
+// dime(9)
+// dime("Dimitar is ")
+
+// function volcube(l, w, h){
+//     console.log("volume is " + (l* w * h) +"m3")
+// }
+// volcube(6,4,3,)
+
+function volcube(l){
+    // return l + 2;
+    return (l + l)
+}
+volcube(5)
+console.log(volcube(5) + " Ova korista na return")
+var result = volcube(7)
+console.log(result)
+
+
+
+
+// function jas(a, b){
+//     return ((a = a+b) + a + 3) - result;
+// }
+
+// jas(3,5)
+// console.log(jas(3,5))
+
+// function capitalize(str){
+//     return str.charAt(0).toUpperCase() + str.slice(1);
+// }
+// var city = "paris";
+// var city2 = "skopje";
+// var capital = capitalize(city + " and " + capitalize(city2) + " and " + result)
+// console.log(capital)
+
+// function pav(a, b){
+//     return  a + b 
+// }
+
+// var c = pav(6,9)
+// console.log(c)
+
+
+// function capitalize(str){
+//     if( typeof str === "number"){
+//         return str
+//     }
+//     return str.charAt(0).toUpperCase() + str.slice(1) + " Not always f should have return";
+// }
+// var city = "london";
+// var capital = capitalize(city)
+// console.log(capital)
+
+// var num = 37;
+// var capital = capitalize(num +"and"+(num+5))
+// console.log(capital)
+
+// var num = 37;
+// var capital = capitalize(num +"and"+(num+5) + ", " + (num  + 5)+ " is sum of " + num + " and 5" );
+// console.log(capital)
+
+
+// console.log(typeof 42);
+// console.log(typeof "Dimitar");
