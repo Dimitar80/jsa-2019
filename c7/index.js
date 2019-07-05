553ac69c53a08ebd07b8c5833fcc804890c4d24f
Dimitar80 <designcrafts.mk@gmail.com>
2019-07-05 11:57:35 +0200

    patch-proba

--- c7/index.js
+++ c7/index.js
@@ -1,9 +1,15 @@
 // console.log("Zdravo")
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var students = require('./handlers/students');
 var calculator = require('./handlers/calculator');
+var food = require('./handlers/food');
+
 var api = express();
+api.use(bodyParser.json());
+api.use(express.static('www'));
+
 
 api.get('/students', students.GetAllStudents);
 api.get('/students/:idf', students.GetStudentByID);
@@ -12,7 +18,13 @@ api.get('/students/:id/grades/:sub', students.GetStudentSubjectGrades);
 
 
 api.get('/calc/:op/:a/:b', calculator.Calculator);
-api.get('/calc/:op/:a/:b', calculator.Calculator);
+
+api.get('/food', food.GetAllFood);
+api.get('/food/:id', food.GetSingleFood);
+api.post('/food', food.CreateNewFood);
+api.put('/food/:id', food.UpdateFood);
+api.patch('/food/:id/:ki', food.PartialUpdateFood);
+api.delete('/food/:id', food.DeleteFood);
 
 
 api.listen(8080, (err)=>{
@@ -22,3 +34,5 @@ api.listen(8080, (err)=>{
     }
     console.log('Server started successfully');
 });
+
+
